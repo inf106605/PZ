ff8f588078b208870f49490308119028f4fe2332
Krzysztof <k.jerzynski1@gmail.com>
2016-03-22 13:03:42 +0100

    + implementation gui for MainWindow + create image logo

--- SkyCrab/SkyCrab/MainWindow.xaml.cs
+++ SkyCrab/SkyCrab/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using SkyCrab.Classess;
 
 namespace SkyCrab
 {
@@ -28,6 +29,31 @@ namespace SkyCrab
         private void button_test_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Test application");
+        }
+
+        private void Image_Loaded(object sender, RoutedEventArgs e)
+        {
+            // ... Create a new BitmapImage.
+            BitmapImage b = new BitmapImage();
+            b.BeginInit();
+            b.UriSource = new Uri("crab_logo.png", UriKind.Relative);
+            b.CacheOption = BitmapCacheOption.OnLoad;
+            b.EndInit();
+
+            // ... Get Image reference from sender.
+            var image = sender as Image;
+            // ... Assign Source.
+            image.Source = b;
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
