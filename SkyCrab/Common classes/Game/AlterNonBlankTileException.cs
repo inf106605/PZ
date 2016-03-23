58ad1ebbc1b363d1f4d0b7ee0bc81df01f01406f
Piotr Siupa <piotrsiupa@gmail.com>
2016-03-23 21:19:54 +0100

    Add class 'Tile'.

---
+++ SkyCrab/Common classes/Game/AlterNonBlankTileException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Common_classes.Game
+{
+    class AlterNonBlankTileException : SkyCrabException
+    {
+
+        public AlterNonBlankTileException() :
+            base()
+        {
+        }
+        
+        public AlterNonBlankTileException(String message) :
+            base(message)
+        {
+        }
+        
+        public AlterNonBlankTileException(String message, Exception innerException) :
+            base(message, innerException)
+        {
+        }
+
+    }
+}
---
+++ SkyCrab/Common classes/Game/Tile.cs
@@ -0,0 +1,60 @@
+﻿namespace Common_classes.Game
+{
+    class Tile
+    {
+
+        private bool blank;
+        private char letter;
+
+
+        public bool Blank
+        {
+            get
+            {
+                return blank;
+            }
+        }
+
+        public char Letter
+        {
+            get
+            {
+                return letter;
+            }
+            set
+            {
+                if (blank == false)
+                    throw new AlterNonBlankTileException();
+                letter = value;
+            }
+        }
+        
+
+        /// <summary> Blanks and letters. </summary>
+        public Tile(bool blank, char letter)
+        {
+            this.blank = blank;
+            this.letter = letter;
+        }
+
+        /// <summary> Blanks only. </summary>
+        public Tile()
+        {
+            this.blank = true;
+            this.letter = '\0';
+        }
+
+        /// <summary> Letters only. </summary>
+        public Tile(char letter)
+        {
+            this.blank = false;
+            this.letter = letter;
+        }
+
+        public bool isLetterEmpty()
+        {
+            return letter == '\0';
+        }
+
+    }
+}
