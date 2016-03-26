e925e11c9cf1359a97a48ec1187f71cdaa749dec
Piotr Siupa <piotrsiupa@gmail.com>
2016-03-26 20:56:24 +0100

    Add classes 'Board' & 'StandardBoard'.

---
+++ SkyCrab/Common classes/Game/Board.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+
+namespace Common_classes.Game
+{
+    public struct PositionOnBoard
+    {
+        public int x;
+        public int y;
+
+        public PositionOnBoard(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public enum SquareType
+    {
+        normal = 0,
+        start = 1,
+        letter2 = 2,
+        letter3 = 3,
+        word2 = 4,
+        word3 = 5
+    }
+
+    public class NoSuchSquareOnBoardException : SkyCrabException
+    {
+
+        public NoSuchSquareOnBoardException() :
+            base()
+        {
+        }
+
+    }
+
+    public class SquareOnBoardIsOccupiedException : SkyCrabException
+    {
+
+        public SquareOnBoardIsOccupiedException() :
+            base()
+        {
+        }
+
+    }
+
+    public abstract class Board
+    {
+
+        public abstract bool Rectangle { get; }
+
+        public abstract PositionOnBoard LeftTop { get; }
+        public abstract PositionOnBoard RightBottom { get; }
+
+        public abstract IList<PositionOnBoard> Squares { get; }
+
+        public abstract PositionOnBoard StartSquare { get; }
+
+
+        public abstract bool IsSquare(PositionOnBoard position);
+
+        public abstract SquareType GetQuareType(PositionOnBoard position);
+
+        public abstract void PutTile(Tile tile, PositionOnBoard position);
+
+        public abstract Tile GetTile(PositionOnBoard position);
+
+        public abstract string getSquareID(PositionOnBoard position, bool horizontal);
+
+        protected static string getStandardSquareID(PositionOnBoard position, bool horizontal)
+        {
+            string number = (position.x + 1) + "";
+            char letter = (char) (((int) 'A') + position.y);
+            if (horizontal)
+                return letter + number;
+            else
+                return number + letter;
+        }
+
+    }
+}
---
+++ SkyCrab/Common classes/Game/StandardBoard.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common_classes.Game
+{
+    class StandardBoard : Board
+    {
+
+        private static readonly List<PositionOnBoard> squares;
+
+        private static readonly SquareType[][] squareTypes = new SquareType[8][]
+                {
+					new SquareType[8] { SquareType.word3,   SquareType.normal,  SquareType.normal,  SquareType.letter2, SquareType.normal,  SquareType.normal,  SquareType.normal,  SquareType.word3 },
+					new SquareType[8] { SquareType.normal,  SquareType.word2,   SquareType.normal,  SquareType.normal,  SquareType.normal,  SquareType.letter3, SquareType.normal,  SquareType.normal },
+					new SquareType[8] { SquareType.normal,  SquareType.normal,  SquareType.word2,   SquareType.normal,  SquareType.normal,  SquareType.normal,  SquareType.letter2, SquareType.normal },
+					new SquareType[8] { SquareType.letter2, SquareType.normal,  SquareType.normal,  SquareType.word2,   SquareType.normal,  SquareType.normal,  SquareType.normal,  SquareType.letter2 },
+					new SquareType[8] { SquareType.normal,  SquareType.normal,  SquareType.normal,  SquareType.normal,  SquareType.word2,   SquareType.normal,  SquareType.normal,  SquareType.normal },
+					new SquareType[8] { SquareType.normal,  SquareType.letter3, SquareType.normal,  SquareType.normal,  SquareType.normal,  SquareType.letter3, SquareType.normal,  SquareType.normal },
+					new SquareType[8] { SquareType.normal,  SquareType.normal,  SquareType.letter2, SquareType.normal,  SquareType.normal,  SquareType.normal,  SquareType.letter2, SquareType.normal },
+					new SquareType[8] { SquareType.word3,   SquareType.normal,  SquareType.normal,  SquareType.letter2, SquareType.normal,  SquareType.normal,  SquareType.normal,  SquareType.start }
+				};
+
+        private Tile[][] tiles;
+
+
+        static StandardBoard()
+        {
+            squares = new List<PositionOnBoard>(15 * 15);
+            for (int i = 0; i != 14; ++i)
+                for (int j = 0; j != 14; ++j)
+                    squares.Add(new PositionOnBoard(i, j));
+        }
+
+
+        public override bool Rectangle
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public override PositionOnBoard LeftTop
+        {
+            get
+            {
+                return new PositionOnBoard(0, 0);
+            }
+        }
+        public override PositionOnBoard RightBottom
+        {
+            get
+            {
+                return new PositionOnBoard(14, 14);
+            }
+        }
+
+        public override IList<PositionOnBoard> Squares
+        {
+            get
+            {
+                return squares;
+            }
+        }
+
+        public override PositionOnBoard StartSquare
+        {
+            get
+            {
+                return new PositionOnBoard(7, 7);
+            }
+        }
+        
+
+        public StandardBoard()
+        {
+            tiles = new Tile[15][]
+                    {
+                        new Tile[15],
+                        new Tile[15],
+                        new Tile[15],
+                        new Tile[15],
+                        new Tile[15],
+                        new Tile[15],
+                        new Tile[15],
+                        new Tile[15],
+                        new Tile[15],
+                        new Tile[15],
+                        new Tile[15],
+                        new Tile[15],
+                        new Tile[15],
+                        new Tile[15],
+                        new Tile[15]
+                    };
+        }
+
+        public override bool IsSquare(PositionOnBoard position)
+        {
+            return position.x >= 0 && position.x < 15 &&
+                    position.y >= 0 && position.y < 15;
+        }
+
+        public override SquareType GetQuareType(PositionOnBoard position)
+        {
+            if (!IsSquare(position))
+                throw new NoSuchSquareOnBoardException();
+            int x = (position.x <= 7) ? position.x : (14 - position.x);
+            int y = (position.y <= 7) ? position.y : (14 - position.y);
+            SquareType squareType = squareTypes[x][y];
+            return squareType;
+        }
+
+        public override void PutTile(Tile tile, PositionOnBoard position)
+        {
+            if (GetTile(position) != null)
+                throw new SquareOnBoardIsOccupiedException();
+            tiles[position.x][position.y] = tile;
+        }
+
+        public override Tile GetTile(PositionOnBoard position)
+        {
+            if (!IsSquare(position))
+                throw new NoSuchSquareOnBoardException();
+            Tile tile = tiles[position.x][position.y];
+            return tile;
+        }
+
+        public override string getSquareID(PositionOnBoard position, bool horizontal)
+        {
+            return getStandardSquareID(position, horizontal);
+        }
+
+    }
+}
