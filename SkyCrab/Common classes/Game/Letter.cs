9f3a712b9f30d3891f5f0880cdbd13b588c601af
Piotr Siupa <piotrsiupa@gmail.com>
2016-03-27 14:35:07 +0200

    Add classes 'Letter', 'LetterSet' & 'PolishLetterSet'.

---
+++ SkyCrab/Common classes/Game/Letter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Common_classes.Game
+{
+    public struct Letter
+    {
+
+        char character;
+
+        uint points;
+
+        public Letter(char character, uint points)
+        {
+            this.character = character;
+            this.points = points;
+        }
+
+    }
+
+    public abstract class LetterSet
+    {
+
+        public static readonly Letter BLANK = new Letter('\0', 0);
+
+
+        public abstract IList<Letter> Letters { get; }
+
+    }
+
+}
