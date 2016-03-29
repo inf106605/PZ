93da589b4f42e241212df4b60c0edf442ed953cf
Piotr Siupa <piotrsiupa@gmail.com>
2016-03-29 15:24:39 +0200

    Add class 'PlayerInLobby'.

---
+++ SkyCrab/Common classes/Lobbies/Players/PlayerInLobby.cs
@@ -0,0 +1,23 @@
+﻿namespace Common_classes.Lobbies.Players
+{
+    class PlayerInLobby
+    {
+
+        //TODO when class 'Player' will be created: private Player player;
+        private bool isReady = false;
+
+
+        public bool IsReady
+        {
+            get
+            {
+                return isReady;
+            }
+            set
+            {
+                isReady = value;
+            }
+        }
+
+    }
+}
