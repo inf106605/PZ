f1e47dd3a75694522c58000e2fd9996d6579f699
Piotr Siupa <piotrsiupa@gmail.com>
2016-03-29 02:07:37 +0200

    Add classes 'Rule', 'TimeRule' & RuleSet'.

---
+++ SkyCrab/Common classes/Lobbies/Rules/RuleSet.cs
@@ -0,0 +1,12 @@
+﻿namespace Common_classes.Lobbies.Rules
+{
+	//Options that are sure to exist for now.
+    public struct RuleSet
+    {
+
+        public TimeRule maxRoundTIme;
+        public Rule<bool> fivesFirst;
+        public Rule<bool> restrictedExchange;
+
+    }
+}
---
+++ SkyCrab/Common classes/Lobbies/Rules/TimeRule.cs
@@ -0,0 +1,10 @@
+﻿namespace Common_classes.Lobbies.Rules
+{
+    public class TimeRule : Rule<uint>
+    {
+
+        public uint minTime;
+        public uint maxTime;
+
+    }
+}
