ae37b9ca9fc11f6581f2574470e6490a868ecf84
kris123456 <k.jerzynski1@gmail.com>
2016-03-30 18:10:54 +0200

    + fixed namespaces with usercontrol + add hyperlink navigate for rules in Registration.xaml

--- SkyCrab/SkyCrab/Classes/Menu/Login.xaml.cs
+++ SkyCrab/SkyCrab/Classes/Menu/Login.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using SkyCrab.Menu;
 
 namespace SkyCrab.Classes.Menu
 {
@@ -27,7 +16,7 @@ namespace SkyCrab.Classes.Menu
 
         private void ButtonLoginReturn_Click(object sender, RoutedEventArgs e)
         {
-            Switcher.Switch(new SkyCrab.Menu.MainMenu());
+            Switcher.Switch(new MainMenu());
         }
     }
 }
