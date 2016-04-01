1788d5d10ca30eed2ffb19173bf351ef839ad9be
kris123456 <k.jerzynski1@gmail.com>
2016-04-02 00:21:54 +0200

    + gui for cart PlayAsLoggedPlayer.xaml

--- SkyCrab/SkyCrab/Classes/Menu/LoggedPlayer/PlayAsLoggedPlayer.xaml.cs
+++ SkyCrab/SkyCrab/Classes/Menu/LoggedPlayer/PlayAsLoggedPlayer.xaml.cs
@@ -24,5 +24,10 @@ namespace SkyCrab.Classes.Menu
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Switcher.Switch(new MainMenuLoggedPlayer());
+        }
     }
 }
