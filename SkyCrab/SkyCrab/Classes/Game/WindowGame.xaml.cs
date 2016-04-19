8af58129b569f10b3b4a6b8235ec4fd86ddbe783
Krzysztof <k.jerzynski1@gmail.com>
2016-04-19 13:05:13 +0200

    + adding listview for display players in WindowGame.xaml + adding class ScrabblePlayers.cs + fixed bootstrap in WindowGame.xaml

--- SkyCrab/SkyCrab/Classes/Game/WindowGame.xaml.cs
+++ SkyCrab/SkyCrab/Classes/Game/WindowGame.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SkyCrab.Menu;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
@@ -22,12 +23,28 @@ namespace SkyCrab.Classes.Game
     /// </summary>
     public partial class WindowGame : UserControl
     {
+        private List<ScrabblePlayers> ScrabblePlayers = null;
 
         public WindowGame()
         {
             InitializeComponent();
+            InitBinding();
             DataContext = new ScrabbleGame();
-        }   
+        }
 
+        private void InitBinding()
+        {
+            ScrabblePlayers = new List<ScrabblePlayers>();
+            ScrabblePlayers.Add(new ScrabblePlayers("( Ty ) pleban325", 60, "0:05:59", 4));
+            ScrabblePlayers.Add(new ScrabblePlayers("paterak195", 45, "0:05:59", 5));
+            ScrabblePlayers.Add(new ScrabblePlayers("sebaalex", 120, "0:05:59", 7));
+            ScrabblePlayers.Add(new ScrabblePlayers("ziomeczek", 15, "0:05:59", 2));
+            ListPlayers.ItemsSource = ScrabblePlayers;
+        }
+
+        private void ExitGame_Click(object sender, RoutedEventArgs e)
+        {
+            Switcher.Switch(new MainMenu());
+        }
     } 
 }
