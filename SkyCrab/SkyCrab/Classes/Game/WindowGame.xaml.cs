b50ed948024416493484ed57022e415c43d2011a
Krzysztof <k.jerzynski1@gmail.com>
2016-04-23 21:09:21 +0200

    + Simulation Play - remove Tiles from Observable Collection and ListBox + Fixed Drag And Drop on Rack

--- SkyCrab/SkyCrab/Classes/Game/WindowGame.xaml.cs
+++ SkyCrab/SkyCrab/Classes/Game/WindowGame.xaml.cs
@@ -1,4 +1,6 @@
-﻿using SkyCrab.Menu;
+﻿using SkyCrab.Common_classes.Games.Racks;
+using SkyCrab.Common_classes.Games.Tiles;
+using SkyCrab.Menu;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -39,12 +41,45 @@ namespace SkyCrab.Classes.Game
             ScrabblePlayers.Add(new ScrabblePlayers("paterak195", 45, "0:05:32", 5));
             ScrabblePlayers.Add(new ScrabblePlayers("sebaalex", 120, "0:06:47", 7));
             ScrabblePlayers.Add(new ScrabblePlayers("ziomeczek", 15, "0:07:31", 2));
+            Rack rack = new Rack(); // klasa piotra , do zrobienia
             ListPlayers.ItemsSource = ScrabblePlayers;
         }
 
         private void ExitGame_Click(object sender, RoutedEventArgs e)
         {
             Switcher.Switch(new MainMenu());
+        }
+
+        private void Play_Click(object sender, RoutedEventArgs e)
+        {
+            // symulacja wyłożenia płytek
+
+            List<int> listTiles = new List<int>();
+
+            foreach (var item in listViewRack.SelectedItems)
+            {
+                var x = item.GetType().GetProperty("Id").GetValue(item, null).ToString();
+                listTiles.Add(int.Parse(x));
+                MessageBox.Show(x);
+            }
+
+            // sortowanie listy z id elementów 
+
+            listTiles.Sort();
+
+            // wyświetlenie id usuwanych elementów
+
+            string a = "";
+
+            for (int i = 0; i < listTiles.Count; i++)
+                a += listTiles[0] + " ";
+
+            MessageBox.Show("Lista zaznaczonych: " + a);
+
+            // usunięcie z kolekcji Rack zaznaczonych płytek 
+
+            for (int i = 0; i < listTiles.Count; i++)
+                ScrabbleRack.RackTiles.Remove(ScrabbleRack.RackTiles.Where(temp => temp.Id == listTiles[i]).Single());
         }
     } 
 }
