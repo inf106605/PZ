4f3c380c46b2474b46b09d31e1760146bfe1951f
Piotr Siupa <piotr.siupa@student.put.poznan.pl>
2016-04-26 15:21:30 +0200

    Change message IDs. Ping & Pong.

---
+++ SkyCrab/Connection/PresentationLayer/Messages/Common/PingMsg.cs
@@ -0,0 +1,43 @@
+﻿namespace SkyCrab.Connection.PresentationLayer.Messages.Menu
+{
+    /// <summary>
+    /// <para>Sender: Server &amp; Client</para>
+    /// <para>ID: <see cref="MessageId.PING"/></para>
+    /// <para>Data type: [none]</para>
+    /// <para>Passible answers: <see cref="PongMsg"/></para>
+    /// </summary>
+    public sealed class PingMsg : AbstractMessage
+    {
+
+        public override MessageId Id
+        {
+            get { return MessageId.PING; }
+        }
+
+        internal override bool Answer
+        {
+            get { return false; }
+        }
+
+
+        internal override object Read(MessageConnection connection)
+        {
+            return null;
+        }
+
+        public static MessageConnection.MessageInfo? SyncPostPing(MessageConnection connection, int timeout)
+        {
+            return SyncPost((callback, state) => AsyncPostPing(connection, callback, state), timeout);
+        }
+
+        public static void AsyncPostPing(MessageConnection connection, MessageConnection.AnswerCallback callback, object state = null)
+        {
+            MessageConnection.MessageProcedure messageProcedure = (writingBlock) =>
+            {
+                connection.SetAnswerCallback(writingBlock, callback, state);
+            };
+            connection.PostMessage(MessageId.PING, messageProcedure);
+        }
+
+    }
+}
---
+++ SkyCrab/Connection/PresentationLayer/Messages/Common/PongMsg.cs
@@ -0,0 +1,37 @@
+﻿namespace SkyCrab.Connection.PresentationLayer.Messages.Menu
+{
+    /// <summary>
+    /// <para>Sender: Server &amp; Client</para>
+    /// <para>ID: <see cref="MessageId.PONG"/></para>
+    /// <para>Data type: [none]</para>
+    /// <para>Passible answers: [none]</para>
+    /// </summary>
+    public sealed class PongMsg : AbstractMessage
+    {
+
+        public override MessageId Id
+        {
+            get { return MessageId.PONG; }
+        }
+
+        internal override bool Answer
+        {
+            get { return true; }
+        }
+
+
+        internal override object Read(MessageConnection connection)
+        {
+            return null;
+        }
+
+        public static void AsyncPostPong(MessageConnection connection)
+        {
+            MessageConnection.MessageProcedure messageProcedure = (writingBlock) =>
+            {
+            };
+            connection.PostMessage(MessageId.PONG, messageProcedure);
+        }
+
+    }
+}
