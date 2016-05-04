f26a6746c0957823fae8f5eb6b37497a5cad64cf
Krzysztof <k.jerzynski1@gmail.com>
2016-05-04 18:55:16 +0200

    + fixed PlayAsLoggedPlayer.xaml and function binding data

--- SkyCrab/SkyCrab/Classes/Menu/LoggedPlayer/PlayAsLoggedPlayer.xaml.cs
+++ SkyCrab/SkyCrab/Classes/Menu/LoggedPlayer/PlayAsLoggedPlayer.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using SkyCrab.Classes.Menu.LoggedPlayer;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,6 +22,13 @@ namespace SkyCrab.Classes.Menu
     /// </summary>
     public partial class PlayAsLoggedPlayer : UserControl
     {
+
+        ObservableCollection<String> typeOfRoomLabels;
+        ObservableCollection<String> minTimeLimitLabels;
+        ObservableCollection<String> maxTimeLimitLabels;
+        ObservableCollection<String> minCountPlayersLabels;
+        ObservableCollection<String> maxCountPlayersLabels;
+
         public PlayAsLoggedPlayer()
         {
             InitializeComponent();
@@ -34,6 +42,80 @@ namespace SkyCrab.Classes.Menu
         private void RoomCreateButton_Click(object sender, RoutedEventArgs e)
         {
             Switcher.Switch(new CreateRoomForLoggedPlayers());
+        }
+
+        private void textBoxSearchRoom_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void typeOfRoom_Loaded(object sender, RoutedEventArgs e)
+        {
+            typeOfRoomLabels = new ObservableCollection<String>();
+
+            typeOfRoomLabels.Add("publiczny");
+            typeOfRoomLabels.Add("znajomi");
+
+            typeOfRoom.ItemsSource = typeOfRoomLabels;
+            typeOfRoom.SelectedIndex = 0;
+        }
+
+        private void minTimeLimit_Loaded(object sender, RoutedEventArgs e)
+        {
+            minTimeLimitLabels = new ObservableCollection<String>();
+
+            for(int i=5; i <=15; i+=5)
+            {
+                minTimeLimitLabels.Add(i.ToString());
+            }
+            for(int i=15; i <=60; i+=15)
+            {
+                minTimeLimitLabels.Add(i.ToString());
+            }
+            minTimeLimitLabels.Add("Brak limitu");
+
+            minTimeLimit.ItemsSource = minTimeLimitLabels;
+            minTimeLimit.SelectedIndex = 0;
+        }
+
+        private void maxTimeLimit_Loaded(object sender, RoutedEventArgs e)
+        {
+            maxTimeLimitLabels = new ObservableCollection<String>();
+
+            for (int i = 5; i <= 15; i += 5)
+            {
+                maxTimeLimitLabels.Add(i.ToString());
+            }
+            for (int i = 15; i <= 60; i += 15)
+            {
+                maxTimeLimitLabels.Add(i.ToString());
+            }
+            maxTimeLimitLabels.Add("Brak limitu");
+            maxTimeLimit.ItemsSource = minTimeLimitLabels;
+            maxTimeLimit.SelectedIndex = 0;
+
+        }
+
+        private void minCountPlayers_Loaded(object sender, RoutedEventArgs e)
+        {
+            minCountPlayersLabels = new ObservableCollection<String>();
+            for(int i=1; i <=4; i++)
+            {
+                minCountPlayersLabels.Add(i.ToString());
+            }
+            minCountPlayers.ItemsSource = minCountPlayersLabels;
+            minCountPlayers.SelectedIndex = 0;
+        }
+
+        private void maxCountPlayers_Loaded(object sender, RoutedEventArgs e)
+        {
+            maxCountPlayersLabels = new ObservableCollection<String>();
+            for (int i = 1; i <= 4; i++)
+            {
+                maxCountPlayersLabels.Add(i.ToString());
+            }
+            maxCountPlayers.ItemsSource = maxCountPlayersLabels;
+            maxCountPlayers.SelectedIndex = 0;
         }
     }
 }
