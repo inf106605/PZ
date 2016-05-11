2a91f183d6a264ae430d8cce4891791d3bd812e3
Piotr Siupa <piotr.siupa@student.put.poznan.pl>
2016-05-12 01:02:50 +0200

    Repair canceling starting the game. Change start time to 10 seconds.

--- SkyCrab/SkyCrabServer/ServerClasses/ServerRoom.cs
+++ SkyCrab/SkyCrabServer/ServerClasses/ServerRoom.cs
@@ -29,7 +29,7 @@ namespace SkyCrabServer.ServerClasses
                     return;
             if (startGameTimer != null)
                 return;
-            Task.Factory.StartNew(StartGameTimerTaskBody);
+            startGameTimer = Task.Factory.StartNew(StartGameTimerTaskBody);
         }
 
         public void CancelStartingGame()
@@ -38,37 +38,42 @@ namespace SkyCrabServer.ServerClasses
                 return;
             startGameSemaphore.Release();
             startGameTimer.Wait();
-            startGameSemaphore.WaitOne(0);
+            if (startGameSemaphore.WaitOne(0))
+                return;
             startGameTimer = null;
+            return;
         }
 
         private void StartGameTimerTaskBody()
         {
-            if (startGameSemaphore.WaitOne(5000))
+            if (startGameSemaphore.WaitOne(10000))
                 return;
-            Globals.dataLock.AcquireWriterLock(-1);
-            try
-            {
-                foreach (PlayerInRoom playerInRoom in room.Players)
-                    playerInRoom.IsReady = false;
-                StartGame();
-            }
-            finally
-            {
-                Globals.dataLock.ReleaseWriterLock();
-            }
+            Task.Factory.StartNew(StartGame);
         }
 
         private void StartGame()
         {
-            Game game = new Game(1234, room, false);//TODO
-            foreach (PlayerInRoom playerInRoom in room.Players)
+            Globals.dataLock.AcquireWriterLock(-1);
+            try
             {
-                ServerPlayer otherServerPlayer; //Schrödinger Variable
-                Globals.players.TryGetValue(playerInRoom.Player.Id, out otherServerPlayer);
-                if (otherServerPlayer == null)  //WTF!?
-                    throw new Exception("Whatever...");
-                GameStartedMsg.AsyncPost(otherServerPlayer.connection, game.Id);
+                startGameTimer = null;
+                foreach (PlayerInRoom playerInRoom in room.Players)
+                    if (!playerInRoom.IsReady)
+                        return;
+                Game game = new Game(1234, room, false);//TODO
+                foreach (PlayerInRoom playerInRoom in room.Players)
+                {
+                    playerInRoom.IsReady = false;
+                    ServerPlayer otherServerPlayer; //Schrödinger Variable
+                    Globals.players.TryGetValue(playerInRoom.Player.Id, out otherServerPlayer);
+                    if (otherServerPlayer == null)  //WTF!?
+                        throw new Exception("Whatever...");
+                    GameStartedMsg.AsyncPost(otherServerPlayer.connection, game.Id);
+                }
+            }
+            finally
+            {
+                Globals.dataLock.ReleaseWriterLock();
             }
         }
 
