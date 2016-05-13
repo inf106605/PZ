433a4238b0de810a735fe449714dd032245c766a
Piotr Siupa <piotr.siupa@student.put.poznan.pl>
2016-05-14 00:11:42 +0200

    Template of class "Dictionary".

---
+++ SkyCrab/Dictionary/Dictionary.cs
@@ -0,0 +1,23 @@
+﻿namespace SkyCrab.Dictionary
+{
+    public sealed class Dictionary
+    {
+
+        private Dictionary()
+        {
+            //TODO
+        }
+
+        public static Dictionary GetPolish()
+        {
+            //TODO
+        }
+
+        public bool IsWordWalid(string word)
+        {
+            //TODO
+            return true;
+        }
+
+    }
+}
